8088a8f2aea05c8bf26d325cb432a5bd8d295429
Eugen Kondratiev <kondratiev.es@gmail.com>
2020-07-27 15:36:30 +0300

    test lambda mongo 1

--- src/api.js
+++ src/api.js
@@ -17,7 +17,39 @@ mainRouter.get('/info', async (req, res) => {
     console.log("#### GET info Route");
 
     const _info = await require('../mongo/get-mongo-data')('info', { players: { $exists: true } });
+    console.log("### info", _info)
     res.json(_info)
+})
+
+
+mainRouter.get('/db-test', async (req, res) => {
+    console.log("#### GET db-test Route");
+    const MongoClient = require("mongodb").MongoClient;
+    const dotenv = require('dotenv');
+    dotenv.config();
+
+    const URI = process.env.MONGODB_URI;
+    const DB_NAME = process.env.MONGODB_DB_NAME;
+    let _data = null;
+    let client = null;
+
+    try {
+        client = await MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        // const dataBase = await client.db(DB_NAME);
+        // _data = await dataBase.collection('info').find({ players: { $exists: true } }).toArray();
+        _data = clint.db;
+        console.log("### info", _data);
+    } catch (error) {
+        console.log("mongo-error : ", error);
+        _data = "mongo-error : " + error;
+    } finally {
+        client.close();
+        // res.json(_data)
+        res.json(_data)
+    }
+
+
 })
 
 mainRouter.get('/env-test', async (req, res) => {
