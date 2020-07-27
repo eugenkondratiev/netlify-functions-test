26e4976dd4c60a8c4d39689a190a80f5df56419a
Eugen Kondratiev <kondratiev.es@gmail.com>
2020-07-27 14:49:08 +0300

    test environment

--- src/api.js
+++ src/api.js
@@ -17,10 +17,22 @@ mainRouter.get('/info', async (req, res) => {
     console.log("#### GET info Route");
     const mongoClient = await require('../mongo/db-mongo')();
     const data2 = await mongoClient._db.collection("info").find({ players: { $exists: true } }).toArray();
-
+    mongoClient.client.close();
     res.json(data2)
 })
 
+mainRouter.get('/env-test', async (req, res) => {
+
+    const dotenv = require('dotenv');
+    dotenv.config();
+
+    console.log("#### GET env-test Route");
+
+    const { MONGODB_URI, MONGODB_DB_NAME } = process.env;
+    const data2 = { MONGODB_URI, MONGODB_DB_NAME }
+    res.json(data2)
+
+})
 
 mainRouter.get('/', (req, res) => {
     console.log("#### GET main Route")
