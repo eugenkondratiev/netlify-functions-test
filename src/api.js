2f75c8965a0349d0c6a559b0c435c7805d6f6a26
Eugen Kondratiev <kondratiev.es@gmail.com>
2020-07-27 11:48:47 +0300

    add mongodb test

--- src/api.js
+++ src/api.js
@@ -4,22 +4,31 @@ const { Router } = require('express');
 
 const app = express();
 
-const mainRouter = express.Router() ;
+const mainRouter = express.Router();
 
-mainRouter.get('/pefl', (req,res)=>{
+mainRouter.get('/pefl', (req, res) => {
     console.log("#### GET main Route")
     res.json({
         "msg": "PEFL is the best"
     })
 })
 
-mainRouter.get('/', (req,res)=>{
+mainRouter.get('/info', async (req, res) => {
+    console.log("#### GET info Route");
+    const mongoClient = await require('../mongo/db-mongo')();
+    const data2 = await mongoClient._db.collection("info").find({ players: { $exists: true } }).toArray();
+
+    res.json(data2)
+})
+
+
+mainRouter.get('/', (req, res) => {
     console.log("#### GET main Route")
     res.json({
         "msg": "This API works on netlify"
     })
 })
 
-app.use('/.netlify/functions/api',mainRouter);
+app.use('/.netlify/functions/api', mainRouter);
 
 module.exports.handler = serverless(app)
